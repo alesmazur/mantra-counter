a282b71cc3cafb158f9e330255b4ccf7bdced5d3
Ales Mazur <alesmazurphotography@gmail.com>
2022-10-22 11:31:48 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div className="times">
+          You read mantra <p>{count}</p> times.
+        </div>
+        <div className="buttons">
+          <button
+            className="button"
+            onClick={() => {
+              setCount(count + 1);
+            }}
+          >
+            Add one time
+          </button>
+          <button className="button" onClick={() => setCount(0)}>
+            Reset
+          </button>
+        </div>
       </header>
     </div>
   );
